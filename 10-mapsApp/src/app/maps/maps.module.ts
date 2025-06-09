4f518e6a9cc9627215061e12414998d2e5b93a23
Ferla04 <fer.velas135@gmail.com>
2025-06-09 10:49:21 -0500

    feat: agregar componente de rango de zoom con funcionalidad de zoom y seguimiento de coordenadas

--- 10-mapsApp/src/app/maps/maps.module.ts
+++ 10-mapsApp/src/app/maps/maps.module.ts
@@ -5,9 +5,15 @@ import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
+import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
 
 @NgModule({
-  declarations: [MiniMapComponent, SideMenuComponent, MapsLayoutComponent],
+  declarations: [
+    MiniMapComponent,
+    SideMenuComponent,
+    MapsLayoutComponent,
+    ZoomRangePageComponent,
+  ],
   imports: [CommonModule, MapsRoutingModule],
 })
 export class MapsModule {}
--- 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,10 +1,13 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
   ElementRef,
+  OnDestroy,
+  signal,
   ViewChild,
 } from '@angular/core';
-import { Map } from 'maplibre-gl';
+import { Map, LngLat } from 'maplibre-gl';
 
 @Component({
   selector: 'maps-zoom-range-page',
@@ -12,21 +15,68 @@ import { Map } from 'maplibre-gl';
   styleUrls: ['./zoom-range-page.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
   @ViewChild('map')
   public mapElement?: ElementRef<HTMLElement>;
+
+  public map?: Map;
+  public zoomLevel = signal(10);
+  public currentLngLat = signal(
+    new LngLat(-74.12895771728677, 4.61637386555978)
+  );
 
   ngAfterViewInit(): void {
     if (!this.mapElement) {
       throw new Error('Map element is not defined');
     }
 
-    new Map({
+    this.map = new Map({
       container: this.mapElement.nativeElement,
       style:
         'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL',
-      center: [-74.2, 4.5],
-      zoom: 5,
+      center: this.currentLngLat(),
+      zoom: this.zoomLevel(),
     });
+
+    this.mapListeners();
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
+
+  mapListeners() {
+    if (!this.map) {
+      throw new Error('Map is not initialized');
+    }
+
+    this.map.on('zoom', () => {
+      this.zoomLevel.set(this.map!.getZoom() ?? 0);
+    });
+
+    this.map.on('zoomend', () => {
+      if (this.map!.getZoom() < 18) return;
+      this.map!.setZoom(18);
+    });
+
+    this.map.on('moveend', () => {
+      this.currentLngLat.set(this.map!.getCenter());
+    });
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  onZoomChange(value: number) {
+    if (!this.map) {
+      throw new Error('Map is not initialized');
+    }
+
+    this.map.setZoom(value);
   }
 }
