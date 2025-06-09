7171fc19be04b6e90ad8a5f9f9970668df14df5a
Ferla04 <fer.velas135@gmail.com>
2025-06-09 08:33:25 -0500

    feat: agregar soporte para MapLibre GL y crear página de pantalla completa

--- 10-mapsApp/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,7 +1,32 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ViewChild,
+  type ElementRef,
+} from '@angular/core';
+import { Map } from 'maplibre-gl';
 
 @Component({
   templateUrl: './full-screen-page.component.html',
+  styleUrls: ['./full-screen-page.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FullScreenPageComponent {}
+export class FullScreenPageComponent implements AfterViewInit {
+  @ViewChild('map')
+  public mapElement?: ElementRef<HTMLElement>;
+
+  ngAfterViewInit(): void {
+    if (!this.mapElement) {
+      throw new Error('Map element is not defined');
+    }
+
+    new Map({
+      container: this.mapElement?.nativeElement,
+      style:
+        'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL',
+      center: [-74.2, 4.5],
+      zoom: 5,
+    });
+  }
+}
